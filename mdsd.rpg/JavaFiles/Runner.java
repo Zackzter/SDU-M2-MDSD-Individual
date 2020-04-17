fede252233798eb7a48b112366572503a5f9ba7e
Zackzter <jorge.olesen@hotmail.com>
2020-04-17 18:08:43 +0200

    coments Pog

--- mdsd.rpg/JavaFiles/Runner.java
+++ mdsd.rpg/JavaFiles/Runner.java
@@ -31,35 +31,39 @@ public class Runner {
                 System.out.println(game.getCurrentLocation() + ", " + game.getCurrentTeam());
 
                 enemyEntity = addFightingEntity(game, game.getTeam().getTeamByName(game.getCurrentTeam()), enemyEntityNumber);
-
-                System.out.println(game.getBattleEntities());
                 
                 System.out.println("You are in Johto what do you want to do?");
                 System.out.println("Fight or die trying?");
                 boolean fight = false;
+                
+                // prob make a while loop here so dont get stuck if you don't write fight
                 String scannerString = s.nextLine();
-
                 if (scannerString.equals("fight")) {
                     fight = !fight;
                     while (fight) {
 
-                        System.out.println(
-                                "you are against " + enemyEntity.getName() + " choose your move");
+                        System.out.println("You are against " + enemyEntity.getName() + " choose your move");
+                        
+                        // Only needed if more than one entity on the field
                         boolean playerTurn = true;
+                        
+                        // The player will pick the move to use here
                         if(playerTurn){
                             List<String> moves = playerEntity.getMoveNameList();
                             System.out.println(moves);
 
                             boolean pickMove = true;
                             while (pickMove){  
-                                String fS = s.nextLine();
+                                String moveName = s.nextLine();
                                 
-                                if(moves.contains(fS)){
-                                    System.out.println("You used "+ fS + "\n");
+                                // Will check that the user writes pick a move which exists
+                                if(moves.contains(moveName)){
+                                    int power = game.getMove().getMove(moveName).getMoveAttributes().get(0).getIntValue();                                    
+                                    System.out.println("You used "+ moveName + "\n");
                                     // TODO: in xtend file use actual hp hihi
-                                    health = health - game.getMove().getMove(fS).getMoveAttributes().get(0).getIntValue();
-                                    pickMove = !pickMove;
+                                    health = health - power;
                                     System.out.println("Enemy hp: " + health + "\n");
+                                    pickMove = !pickMove;
                                 }
                                 else{
                                     System.out.println("That's not a possible move!");
@@ -67,11 +71,14 @@ public class Runner {
                             }
                         }
                         playerTurn = !playerTurn;
+
+                        // Will check if an enemy entity is dead
                         if(checkDeath(health)){
                             System.out.println(enemyEntity.getName() + " is dead!");
                             health = 50;
-                            enemyEntityNumber++;
                             enemyEntity.setEntityState(EntityState.DEAD);
+
+                            // If the enemy still has entities left, the next one will battle, otherwise the battle with against this team is over.
                             if(!game.getTeam().getTeamByName(game.getCurrentTeam()).isEmpty()){
                                 enemyEntity = addFightingEntity(game, game.getTeam().getTeamByName(game.getCurrentTeam()), enemyEntityNumber);
                             }
@@ -80,23 +87,28 @@ public class Runner {
 
                             }
                         }
+
+                        // Enemy's turn to pick a move, it is picked randomly
                         else{
                             System.out.println("Enemy Turn...");
                             int move = random.nextInt(enemyEntity.getMoveNameList().size());
+                            int enemyPower = game.getMove().getMove(enemyEntity.getMoveNameList().get(move)).getMoveAttributes().get(0).getIntValue();
                             System.out.println(enemyEntity.getName() + " used " + enemyEntity.getMoveNameList().get(move) + "\n");
-                            player_health = player_health - game.getMove().getMove(enemyEntity.getMoveNameList().get(move)).getMoveAttributes().get(0).getIntValue();
+                            player_health = player_health - enemyPower;
                             System.out.println("Player health: " + player_health + "\n");
                             
+                            // It will check if the player's entity is dead
                             if(checkDeath(player_health)){
-                                System.out.println(playerEntity.getName() + " is dead");
+                                System.out.println("Your " + playerEntity.getName() + " is dead");
                                 player_health = 50;
                                 playerEntity.setEntityState(EntityState.DEAD);
+                                
+                                // Will add the next player entity to the fight, if there are no more, it will go to game over
                                 if(!game.getTeam().getPlayerTeam().isEmpty()){
                                     playerEntity = addFightingEntity(game, game.getTeam().getPlayerTeam(), playerEntityNumber);
                                 }
                                 else{
                                     gameOver = !gameOver;
-                                    System.out.println(gameOver);
                                     fight = !fight;
                                 }
                             }
@@ -108,14 +120,15 @@ public class Runner {
 
             }
             else{
-                System.out.println("Fin");
+                if(!gameOver) System.out.println("Fin");
+                else System.out.println("Game Over!");
                 break;
             }
         }
     }
 
+    // Will add everything the user has written in the .rpg file
     public static void setupGame(Game game){
-
         game.getLocation().setLocations(game.getLocation().getLocations());
         if (game.getTypes().isEmpty() && game.getAttributes().isEmpty()) {
             game.addTypes();
@@ -127,6 +140,7 @@ public class Runner {
         }
     }
 
+    // Will add the entity which will fight
     public static Entity addFightingEntity(Game game,List<Entity> entities, int entityNumber){
         Entity entity = null;
         if(!entities.isEmpty()){
@@ -135,10 +149,12 @@ public class Runner {
         return entity;
     }
 
+    // Will check if an entity is dead
     public static boolean checkDeath(int i){
         if (i <= 0){
             return true;
         }
         return false;
     }
+
 }
